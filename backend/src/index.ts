0419a6ee50ca8f4d129147355fd3712165f62f56
joao magnago <joaomagnago.wm@gmail.com>
2026-05-04 21:59:49 -0300

    chore: install and setup prettier as dev depencency

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,44 +1,41 @@
-import { ApolloServer } from '@apollo/server';
-import express from 'express';
-import cors from 'cors';
-import { buildSchema } from 'type-graphql';
-import { AuthResolver } from './resolvers/auth.resolver.js';
+import { ApolloServer } from '@apollo/server'
+import express from 'express'
+import cors from 'cors'
+import { buildSchema } from 'type-graphql'
+import { AuthResolver } from './resolvers/auth.resolver.js'
 
 async function bootstrap() {
-  const app = express();
+  const app = express()
 
   app.use(
     cors({
       origin: 'http://localhost:5173',
       credentials: true,
     }),
-  );
+  )
 
   const schema = await buildSchema({
     resolvers: [AuthResolver],
     validate: false,
     emitSchemaFile: './schema.graphql',
-  });
+  })
 
   const server = new ApolloServer({
     schema,
-  });
+  })
 
-  await server.start();
+  await server.start()
 
-  app.use(
-    '/graphql',
-    express.json(),
-  );
+  app.use('/graphql', express.json())
 
   app.listen(
     {
       port: 4000,
     },
     () => {
-      console.log(`Servidor iniciado na porta 4000`);
+      console.log(`Servidor iniciado na porta 4000`)
     },
-  );
+  )
 }
 
-bootstrap();
+bootstrap()
