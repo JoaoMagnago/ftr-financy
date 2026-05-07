08d96e7c02cdff7a5af8745aa7ab3eb0e88dcbaa
joao magnago <joaomagnago.wm@gmail.com>
2026-05-06 22:06:15 -0300

    feat: implement create user mutation, input and service

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,6 +1,24 @@
+import { CreateUserInput } from '../dtos/input/user.input.js'
 import { prismaClient } from '../infra/database/prisma.js'
 
 export class UserService {
+  async createUser(data: CreateUserInput) {
+    const findUser = await prismaClient.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    })
+    if (findUser) throw new Error('Usuário já cadastrado!')
+
+    return prismaClient.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+    })
+  }
+
   async findUser(id: string) {
     const user = await prismaClient.user.findUnique({
       where: {
