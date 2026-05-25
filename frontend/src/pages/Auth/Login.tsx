14be54b405c98abe98735ebda39a8cb76d91dcef
joao magnago <joaomagnago.wm@gmail.com>
2026-05-24 23:31:32 -0300

    feat(login): add correct styles of login form

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -8,9 +8,14 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from '@/components/ui/input-group'
+import { Lock, Mail } from 'lucide-react'
 
 export function Login() {
   const [email, setEmail] = useState('')
@@ -18,39 +23,47 @@ export function Login() {
 
   return (
     <div className="flex flex-col min-h-[calc(100vh-4rem)] items-center justify-center gap-6">
-      <img src={logo} className="w-64 h-22" />
+      <img src={logo} className="w-32 h-8" />
       <Card className="w-full max-w-md rounded-xl">
-        <CardHeader>
-          <CardTitle className="text-2xl font-bold">
-            Acesse a plataforma
+        <CardHeader className="flex flex-col items-center border-s-red-200 justify-center">
+          <CardTitle className="text-xl font-bold color-foreground">
+            Fazer login
           </CardTitle>
-          <CardDescription>
-            Entre usando seu e-mail e senha cadastrados
-          </CardDescription>
+          <CardDescription>Entre na sua conta para continuar</CardDescription>
         </CardHeader>
         <CardContent>
           <form className="space-y-4">
-            <div>
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="seu@email.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="email">E-mail</Label>
+              <InputGroup data-state={email ? 'filled' : 'empty'}>
+                <InputGroupInput
+                  id="email"
+                  type="email"
+                  placeholder="mail@exemplo.com"
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <InputGroupAddon>
+                  <Mail />
+                </InputGroupAddon>
+              </InputGroup>
             </div>
-            <div>
-              <Label htmlFor="email">Senha</Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="password">Senha</Label>
+              <InputGroup data-state={password ? 'filled' : 'empty'}>
+                <InputGroupInput
+                  id="password"
+                  type="password"
+                  placeholder="Digite sua senha"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputGroupAddon>
+                  <Lock />
+                </InputGroupAddon>
+              </InputGroup>
             </div>
             <Button type="submit" className="w-full">
               Entrar
